43ea6ee4ec2da67d3a3f4d3d63f9bdf849200590
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-27 21:45:29 +0600

    verfy token section added

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+
+// midlleware 
+
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://hotel-fair.web.app',
+        'https://b9a11hotelfairs.netlify.app',
+        'https://hotel-fair-crud.vercel.app'
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+const logger = (req, res, next) => {
+    next()
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token
+    if (!token) {
+        return res.status(401).send({ message: 'unathorized access' })
+    }
+    if (token) {
+        jwt.verify(token, process.env.DB_ACCESS_TOKEN, (err, decoded) => {
+            if (err) {
+                return res.status(401).send({ message: 'unathorized access' })
+            }
+            req.user = decoded
+            next()
+        })
+    }
+}
