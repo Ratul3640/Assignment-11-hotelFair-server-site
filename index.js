06c921c60304cdc7c89b9ad0f0c0b39e43f32605
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-27 21:46:03 +0600

    mongodb added section

--- index.js
+++ index.js
@@ -50,3 +50,18 @@ const verifyToken = (req, res, next) => {
         })
     }
 }
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.kmaa4nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
